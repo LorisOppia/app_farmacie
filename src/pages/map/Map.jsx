66c78e2c6c35c0ccfdb48709e6e597bd133a8037
Carlo Tacchella <tacchella.carlo@gmail.com>
2021-08-05 16:00:06 +0200

    [FIX] typo in error screen

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -59,9 +59,6 @@ export class Map extends Component {
   }
 
   async componentDidMount() {
-
-   
-    
     try {
     
     const res = await Geolocation.getCurrentPosition()
@@ -128,13 +125,12 @@ export class Map extends Component {
             </IonToolbar>
             </IonHeader>
             <IonContent>
-
             <IonList>
             <IonItem>
-            <IonLabel>Errore nell'avvio dell'applicazione</IonLabel>
+            <IonLabel className="ion-text-wrap">Errore nell'avvio dell'applicazione</IonLabel>
             </IonItem>
             <IonItem>
-            <IonLabel>Assicurarsi che il Geolocalizzazione 
+            <IonLabel className="ion-text-wrap">Assicurarsi che il Geolocalizzazione 
             e la connessione internet siano attive</IonLabel>
             </IonItem>
             </IonList>
