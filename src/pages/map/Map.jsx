9e2c6acfadb33e303f208cd79f72d430066f1536
xXAleXPolYXx <ale98sayan@gmail.com>
2021-08-09 16:00:15 +0200

    Connected to GeoServer

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -44,6 +44,12 @@ import quartieri from '../../data/quartieri.json'
 import sponsor from '../../assets/img/sponsor.jpg'
 import { locateSharp } from 'ionicons/icons'
 
+//NUOVI IMPORT
+import L from 'leaflet'
+import $ from "jquery"
+import _proxy from "jquery"
+
+
 const url='http://3.142.202.105:7484'
 
 export class Map extends Component {
@@ -107,6 +113,109 @@ export class Map extends Component {
   }
 
   render() {
+    L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
+
+      onAdd: function (map) {
+        // Triggered when the layer is added to a map.
+        //   Register a click listener, then do all the upstream WMS things
+        L.TileLayer.WMS.prototype.onAdd.call(this, map);
+        map.on('click', this.getFeatureInfo, this);
+      },
+    
+      onRemove: function (map) {
+        // Triggered when the layer is removed from a map.
+        //   Unregister a click listener, then do all the upstream WMS things
+        L.TileLayer.WMS.prototype.onRemove.call(this, map);
+        map.off('click', this.getFeatureInfo, this);
+      },
+    
+      getFeatureInfo: function (evt) {
+        // Make an AJAX request to the server and hope for the best
+        var url = this.getFeatureInfoUrl(evt.latlng),
+            showResults = L.Util.bind(this.showGetFeatureInfo, this);
+        $.ajax({
+          url: url,
+          success: function (data, status, xhr) {
+            var err = typeof data === 'string' ? null : data;
+            showResults(err, evt.latlng, data);
+          },
+          error: function (xhr, status, error) {
+            showResults(error);
+          }
+        });
+      },
+    
+      getFeatureInfoUrl: function (latlng) {
+        // Construct a GetFeatureInfo request URL given a point
+        var point = this._map.latLngToContainerPoint(latlng, this._map.getZoom()),
+            size = this._map.getSize(),
+    
+            params = {
+              request: 'GetFeatureInfo',
+              service: 'WMS',
+              srs: 'EPSG:4326',
+              styles: this.wmsParams.styles,
+              transparent: this.wmsParams.transparent,
+              version: this.wmsParams.version,
+              format: this.wmsParams.format,
+              bbox: this._map.getBounds().toBBoxString(),
+              height: size.y,
+              width: size.x,
+              layers: this.wmsParams.layers,
+              query_layers: this.wmsParams.layers,
+              info_format: 'text/html'
+            };
+    
+        params[params.version === '1.3.0' ? 'i' : 'x'] = point.x;
+        params[params.version === '1.3.0' ? 'j' : 'y'] = point.y;
+    
+        // return this._url + L.Util.getParamString(params, this._url, true);
+    
+        var url = this._url + L.Util.getParamString(params, this._url, true);
+    
+    
+        /**
+         * CORS workaround (using a basic php proxy)
+         *
+         * Added 2 new options:
+         *  - proxy
+         *  - proxyParamName
+         *
+         */
+    
+        // check if "proxy" option is defined (PS: path and file name)
+        if(typeof this.wmsParams.proxy !== "undefined") {
+    
+          // check if proxyParamName is defined (instead, use default value)
+          if(typeof this.wmsParams.proxyParamName !== "undefined")
+            this.wmsParams.proxyParamName = 'url';
+    
+          // build proxy (es: "proxy.php?url=" )
+          _proxy = this.wmsParams.proxy + '?' + this.wmsParams.proxyParamName + '=';
+    
+          url = _proxy + encodeURIComponent(url);
+    
+        }
+    
+        return url;
+    
+      },
+    
+      showGetFeatureInfo: function (err, latlng, content) {
+        if (err) { console.log(err); return; } // do nothing if there's an error
+    
+        // Otherwise show the content in a popup, or something.
+        L.popup({ maxWidth: 800})
+          .setLatLng(latlng)
+          .setContent(content)
+          .openOn(this._map);
+      }
+    });
+    
+    L.tileLayer.betterWms = function (url, options) {
+      return new L.TileLayer.BetterWMS(url, options);
+    };
+    
     const { zoom, locationClicked, showModal } = this.props.map
 
     const centerPosition = () => {
@@ -185,13 +294,21 @@ export class Map extends Component {
               <MapConsumer>
                 {map => {
                   map.setView(this.center)
+                  var wmsfarmacie_url="http://172.16.22.166:8080/geoserver/geoapp/wms?service=WMS&version=1.1.0&request=GetMap&layers=geoapp%3Afarmacie&bbox=10.899255752563477%2C45.38235855102539%2C11.06281566619873%2C45.49650573730469&width=768&height=535&srs=EPSG%3A4326&styles=&format=application/openlayers"
+                  var layer = L.tileLayer.betterWms("http://172.16.22.166:8080/geoserver/wms", {
+                    layers: "geoapp:farmacie",
+                    format: "image/png",
+                    transparent: true,
+                    attribution: "mylayerPARCHI",
+                   });
+                  layer.addTo(map);
                   return null
                 }}
               </MapConsumer>
               <Marker position={this.center}>
                 <Popup>Tu sei qui</Popup>
               </Marker>
-              <LocationMarkers myloc={this.state.farmacie.features}/>
+             
             </MapContainer>
           )}
 
